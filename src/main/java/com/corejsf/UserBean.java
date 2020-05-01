3846a7bc79b790358823aa7cad8ee1cf02df9757
unknown <amar.javid@web.de>
2020-05-01 11:22:36 +0200

    jsf with ajax

--- src/main/java/com/corejsf/UserBean.java
+++ src/main/java/com/corejsf/UserBean.java
@@ -9,12 +9,17 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean(name="user") // or @Named("user")
 @SessionScoped
 public class UserBean implements Serializable {
-   private String name;
+   private String name = "";
    private String password;
 
    public String getName() { return name; }   
    public void setName(String newValue) { name = newValue; }
 
    public String getPassword() { return password; }
-   public void setPassword(String newValue) { password = newValue; }   
+   public void setPassword(String newValue) { password = newValue; }
+
+   public String getGreeting() {
+      if(name.length() == 0) return "";
+      else return "Welcome to JSF + Ajax" + name + "!";
+   }
 }
